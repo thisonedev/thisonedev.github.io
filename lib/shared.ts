f7d9e7739ab93f0a6c2e73b7061096e6a1aad3a6
thisguy <git.occupy385@passmail.com>
2026-06-17 17:55:10 +0700

    update git link

--- lib/shared.ts
+++ lib/shared.ts
@@ -6,6 +6,6 @@ export const docsContentRoute = '/llms.mdx';
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
   user: 'thisonedev',
-  repo: 'portfolio',
+  repo: 'thisonedev.github.io',
   branch: 'master',
 };
